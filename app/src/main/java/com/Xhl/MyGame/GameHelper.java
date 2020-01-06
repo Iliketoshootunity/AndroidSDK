628086bb0b93487e541695cff341a895e45e0847
Iliketoshootunity <837646741@qq.com>
2020-01-06 23:10:43 +0800

    qq登陆
    qq登陆

--- app/src/main/java/com/Xhl/MyGame/GameHelper.java
+++ app/src/main/java/com/Xhl/MyGame/GameHelper.java
@@ -24,6 +24,8 @@ public class GameHelper {
         m_Activity = activity;
     }
 
+    public  final  static  int PLATFORM_MSG_QQLOGINCALLBACK=1;      //qq登陆回调
+    public  final  static  int PLATFORM_MSG_WXLOGINCALLBACK=2;      //微信登陆回调
     //发送平台消息给Unity
     public static  void  SendPlatformMsgToUnity(int iMsgId, int iParam1, int iParam2, int iParam3, String strParam1, String strParam2, String strParam3)
     {
@@ -32,10 +34,27 @@ public class GameHelper {
         UnityPlayer.UnitySendMessage(m_PlatformObj,m_PlatformFunction,jsonStr);
     }
 
+    public  final  static  int  PLATFORM_MSG_QQLOGIN=1;            //qq登陆
+    public  final  static  int  PLATFORM_MSG_QQLOGOUT=2;           //qq注销
+    public  final  static  int  PLATFORM_MSG_WXLOGIN=3;            //微信登陆
+    public  final  static  int  PLATFORM_MSG_WXLOGOUT=4;           //微信注销
     //Unity发送消息给平台
     public  static  void SendUnityMsgToPlatform(int iMsg, int iParam1, int iParam2, int iParam3, String strParam1, String strParam2, String strParam3)
     {
-        //TODO
+        switch (iMsg)
+        {
+            case PLATFORM_MSG_QQLOGIN:
+                TencentQQ.Login();
+                break;
+            case PLATFORM_MSG_QQLOGOUT:
+                TencentQQ.LogOut();
+                break;
+            case PLATFORM_MSG_WXLOGIN:
+                break;
+            case PLATFORM_MSG_WXLOGOUT:
+                break;
+
+        }
     }
 
     //Unity获取平台整形数据
@@ -43,31 +62,48 @@ public class GameHelper {
     {
         return 0;
     }
-
+    public  final  static  int  PLATFORM_MSG_TOTALMEMORY= 1;           //总内存
+    public  final  static  int  PLATFORM_MSG_REMAINGMEMORY= 2;        //剩余内存
+    public  final  static  int  PLATFORM_MSG_USEDMEMORY= 3;           //使用内存
     //Unity获取平台长整形数据
     public  static  long GetLongFromPlatform(int type)
     {
         switch (type)
         {
-            case 1:
+            case PLATFORM_MSG_TOTALMEMORY:
                 return  GetTotalMemory();
-            case 2:
+            case PLATFORM_MSG_REMAINGMEMORY:
                 return  GetRemaingMemory();
-            case  3:
+            case  PLATFORM_MSG_USEDMEMORY:
                 return  GetUsedMemory();
         }
         return  0;
     }
+
 
     //Unity获取平台长整形数据
     public  static  long GetLongFromPlatform2(int type, int iParam1, int iParam2, int iParam3, String strParam1, String strParam2, String strParam3)
     {
         return  0;
     }
-
+    public  final  static  int  PLATFORM_MSG_QQCHECKVAILD= 1;           //qq是否过期
+    public  final  static  int  PLATFORM_MSG_QQREFRESHSESSION= 2;        //qq 刷新并获取票据
+    public  final  static  int  PLATFORM_MSG_WXCHECKVAILD= 3;           //WX是否过期
+    public  final  static  int  PLATFORM_MSG_WXREFRESHSESSION= 4;        //WX 刷新并获取票据
     //Unity获取平台字符串形数据
     public  static  String GetStringFromPlatform(int type)
     {
+        switch (type)
+        {
+            case PLATFORM_MSG_QQCHECKVAILD:
+                return String.valueOf(TencentQQ.CheckAutorVaild()) ;
+            case PLATFORM_MSG_QQREFRESHSESSION:
+                return  TencentQQ.RefreshSession().toString();
+            case  PLATFORM_MSG_WXCHECKVAILD:
+                break;
+            case PLATFORM_MSG_WXREFRESHSESSION:
+                break;
+        }
         return "";
     }
 
--- app/src/main/java/com/Xhl/MyGame/TencentQQ.java
+++ app/src/main/java/com/Xhl/MyGame/TencentQQ.java
@@ -9,6 +9,13 @@ import com.tencent.tauth.Tencent;
 
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 public class TencentQQ {
 
     private  static  Tencent m_Tencent;
@@ -28,8 +35,8 @@ public class TencentQQ {
         m_LoginCallBack =new BaseUiListener();
     }
 
-    //登陆成功
-    public static  void Login(JSONObject jsonObj)
+    //登陆
+    public static  void Login()
     {
         Log.d(m_Tag,"QQ Login");
         m_Tencent.login(m_Activity,"all",m_LoginCallBack);
@@ -38,19 +45,52 @@ public class TencentQQ {
     //登出
     public  static  void LogOut()
     {
-
+        Log.d(m_Tag,"QQ LogOut");
+        m_Tencent.logout(m_Activity);
     }
 
+    //检查当前票据是否有效
+    public  static  boolean CheckAutorVaild()
+    {
+        Log.d(m_Tag,"QQ CheckAutorVaild");
+        return m_Tencent.checkSessionValid(AppId);
+    }
+
+    //获取刷新票据
+    public  static  JSONObject RefreshSession()
+    {
+        JSONObject jsonObj=m_Tencent.loadSession(AppId);
+        if(jsonObj==null)
+        {
+            Login();
+        }
+        else
+        {
+            m_Tencent.initSessionCache(jsonObj);
+        }
+        Log.d(m_Tag,"RefreshSession"+jsonObj.toString());
+        return  SetSelfData(jsonObj);
+
+    }
     //登陆成功回调
-    public  static  void LoaginCallBack(JSONObject jsonObj)
+    public  static  void LoaginCallBack(final JSONObject jsonObj)
     {
         Log.d(m_Tag,"QQ LoaginCallBack");
-        InitOpenIdAndTocken(jsonObj);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                InitOpenIdAndTocken(jsonObj);
+                JSONObject obj=SetSelfData(jsonObj);
+                GameHelper.SendPlatformMsgToUnity(GameHelper.PLATFORM_MSG_QQLOGINCALLBACK,0,0,0,obj.toString(),"","");
+            }
+        });
     }
 
     //登陆之后设置Token等数据
     protected  static void InitOpenIdAndTocken(JSONObject jsonObj)
     {
+        Log.d(m_Tag,"QQ InitOpenIdAndTocken  Init");
         try {
             String token= jsonObj.getString(Constants.PARAM_ACCESS_TOKEN);
             String expires=jsonObj.getString(Constants.PARAM_EXPIRES_IN);
@@ -61,14 +101,89 @@ public class TencentQQ {
             }
             m_Tencent.setAccessToken(token,expires);
             m_Tencent.setOpenId(openId);
-
-
-
         }
         catch (Exception e)
         {
             Log.d(m_Tag,"QQ InitOpenIdAndTocken error;"+e.toString());
         }
+    }
+
+    //对传过来的Json进行统一格式化处理
+    protected  static  JSONObject SetSelfData(JSONObject jsonObj)
+    {
+        Log.d(m_Tag,"SetSelfData");
+        JSONObject obj=new JSONObject();
+        try
+        {
+            String token=jsonObj.getString(Constants.PARAM_ACCESS_TOKEN);
+            String expires=jsonObj.getString(Constants.PARAM_EXPIRES_IN);
+            String openId=jsonObj.getString(Constants.PARAM_OPEN_ID);
+            String paytoken=jsonObj.getString("pay_token");
+            String pf=jsonObj.getString("pf");
+            String pfkey=jsonObj.getString("pfkey");
+            String expirestime=jsonObj.getString(Constants.PARAM_EXPIRES_TIME);
+            String unionid=GetUnionid(token);
+
+            obj.put("openid",openId);
+            obj.put("token",token);
+            obj.put("unionid",unionid);
+            obj.put("refreshtoken","");
+            obj.put("expires",expires);
+            obj.put("paytoken",paytoken);
+            obj.put("pf",pf);
+            obj.put("pfkey",pfkey);
+            obj.put("expirestime",expirestime);
+
+            Log.d(m_Tag,"SetSelfData Data:"+obj.toString());
+        }
+        catch (Exception e)
+        {
+            Log.e(m_Tag,"SetSelfData Error:"+e.toString());
+        }
+        return  obj;
+    }
+
+    protected  static  String GetUnionid(String token)
+    {
+        String unionid="";
+        try
+        {
+            URL url=new URL("https://graph.qq.com/oauth2.0/me?access_token="+token+"&unionid=1");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            //这是一个同步请求，可以直接得到结果
+            int code=connection.getResponseCode();
+            if(code==HttpURLConnection.HTTP_OK)
+            {
+                InputStream inputStream=connection.getInputStream();
+                byte[] data=ReadStream(inputStream);
+                String json=new String(data);
+                json =json.replace("(","").replace(")","").replace("callback","");
+                JSONObject jsonObj=new JSONObject("json");
+                unionid =jsonObj.getString("unionid");
+            }
+        }
+        catch (Exception e)
+        {
+            Log.e(m_Tag,"GetUnionid error:"+ e.toString());
+        }
+        return  unionid;
+    }
+
+    protected static  byte[] ReadStream(InputStream inputStream) throws  Exception
+    {
+        ByteArrayOutputStream bo=new ByteArrayOutputStream();
+        byte[] buffer=new byte[1024];
+        int len=0;
+        while ((len=inputStream.read(buffer))!=-1)
+        {
+            bo.write(buffer,0,len);
+        }
+        bo.close();
+        inputStream.close();
+        return  bo.toByteArray();
     }
 
     public  static  void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -82,5 +197,4 @@ public class TencentQQ {
         }
     }
 
-   // private  static  Tecent m_Tecent;
 }
